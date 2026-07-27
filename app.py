6c496ca9277d12b5799d247d9c14461167a30eaf
Malheiros76 <l_souza.malheiros@hotmail.com>
2026-07-27 13:35:47 -0300

    Update login version to V2.0.4

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ def exportar_ocorrencias_para_pdf(ocorrencias, nome_arquivo):
 
 # --- Login ---
 def pagina_login():
-    st.markdown("## 👤 Login de Usuário - V2.0.3 by Leandro Malheiros")
+    st.markdown("## 👤 Login de Usuário - V2.0.4 by Leandro Malheiros")
     usuario = st.text_input("Usuário").strip()
     senha = st.text_input("Senha", type="password").strip()
 
